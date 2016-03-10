9a9ea0d5208e41897bdce9c0e9f5c71d342ddc9b
SteveSnow <Steven.Snow@outlook.com>
2016-03-10 15:03:45 -0500

    checking things

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   # devise_for :users
   devise_for :users, :controllers => { registrations: 'registrations' }
 root 'home#index'
-# devise_for :users, controllers: {
-#         sessions: 'users/sessions'
-#       }
+
 end
